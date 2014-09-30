b595b61da2c6810dfb95aa8624228fa95902df9a
kazuhira-r <kazuhira.m@gmail.com>
2014-10-01 01:36:47 +0900

    fixes #3

--- velocity4s/src/main/scala/org/velocity4s/ScalaMapGetExecutor.scala
+++ velocity4s/src/main/scala/org/velocity4s/ScalaMapGetExecutor.scala
@@ -6,12 +6,18 @@ import org.apache.velocity.runtime.log.Log
 import org.apache.velocity.runtime.parser.node.MapGetExecutor
 import org.apache.velocity.util.introspection.Introspector
 
-class ScalaMapGetExecutor(log: Log, clazz: Class[_], property: String)
+class ScalaMapGetExecutor(log: Log, introspector: Introspector, clazz: Class[_], property: String)
   extends MapGetExecutor(log, clazz, property) {
+
   override def isAlive: Boolean =
     true
 
-  override def execute(o: AnyRef): AnyRef = {
-    o.asInstanceOf[GenMapLike[String, AnyRef, _]].getOrElse(property, null)
-  }
+  override protected def discover(clazz: Class[_]): Unit =
+    setMethod(introspector.getMethod(clazz, property, Array.empty[AnyRef]))
+
+  override def execute(o: AnyRef): AnyRef =
+    if (getMethod != null)
+      getMethod.invoke(o)
+    else
+      o.asInstanceOf[GenMapLike[String, AnyRef, _]].getOrElse(property, null)
 }
--- velocity4s/src/test/scala/org/velocity4s/Velocity4sScalaClassSpec.scala
+++ velocity4s/src/test/scala/org/velocity4s/Velocity4sScalaClassSpec.scala
@@ -25,7 +25,17 @@ class Velocity4sScalaClassSpec extends FunSpec with Velocity4sSpecSupport {
       merge(template, context) should be ("Hi, My name is Ken!, 15 years old")
     }
 
-    it("Method Invoke") {
+    it("Simple Class2") {
+      val templateAsString = "Hi, My name is $person.name!, $person.age years old"
+
+      val (engine, templateName) = newEngineWithTemplate(templateAsString)
+      val template = engine.getTemplate(templateName)
+      val context = newContext("person" -> new SimplePerson2)
+
+      merge(template, context) should be ("Hi, My name is Taro!, 20 years old")
+    }
+
+    it("Method Invoke with parentheses") {
       val templateAsString = "$greeting.say()"
 
       val (engine, templateName) = newEngineWithTemplate(templateAsString)
@@ -34,6 +44,26 @@ class Velocity4sScalaClassSpec extends FunSpec with Velocity4sSpecSupport {
 
       merge(template, context) should be ("Hello Velocity4s")
     }
+
+    it("Method Invoke no parentheses") {
+      val templateAsString = "$greeting.say"
+
+      val (engine, templateName) = newEngineWithTemplate(templateAsString)
+      val template = engine.getTemplate(templateName)
+      val context = newContext("greeting" -> new Greeting)
+
+      merge(template, context) should be ("Hello Velocity4s")
+    }
+
+    it("Method Invoke with argument") {
+      val templateAsString = "$greeting.say('Velocity')"
+
+      val (engine, templateName) = newEngineWithTemplate(templateAsString)
+      val template = engine.getTemplate(templateName)
+      val context = newContext("greeting" -> new Greeting)
+
+      merge(template, context) should be ("Hello Velocity")
+    }
   }
 }
 
@@ -41,7 +71,15 @@ case class CasePerson(name: String, age: Int)
 
 class SimplePerson(val name: String, val age: Int)
 
+class SimplePerson2 {
+  val name: String = "Taro"
+  var age: Int = 20
+}
+
 class Greeting {
   def say: String =
     "Hello Velocity4s"
+
+  def say(arg: String) =
+    s"Hello $arg"
 }
