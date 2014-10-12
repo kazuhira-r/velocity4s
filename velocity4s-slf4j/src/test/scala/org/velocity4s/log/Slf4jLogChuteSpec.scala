80dc630f57d20e614107d52fa2898805adf30c66
kazuhira-r <kazuhira.m@gmail.com>
2014-10-13 01:46:05 +0900

    Migrate ScalaMock => Mockito, add JBossLogging support

--- velocity4s-slf4j/src/test/scala/org/velocity4s/log/Slf4jLogChuteSpec.scala
+++ velocity4s-slf4j/src/test/scala/org/velocity4s/log/Slf4jLogChuteSpec.scala
@@ -4,52 +4,51 @@ import org.apache.velocity.runtime.log.LogChute
 
 import org.slf4j.Logger
 
-import org.scalamock.scalatest.MockFactory
 import org.scalatest.FunSpec
-import org.scalatest.Matchers._
+import org.scalatest.mock.MockitoSugar
+import org.mockito.Mockito._
+import org.mockito.internal.util.reflection.Whitebox
 
-class Slf4jLogChuteSpec extends FunSpec with MockFactory {
+class Slf4jLogChuteSpec extends FunSpec with MockitoSugar {
   describe("Slf4jLogChute Spec") {
     it("log message") {
       val mockLogger = mock[Logger]
 
-      val slf4jLogChute = new Slf4jLogChute {
-        logger = mockLogger
-      }
-
-      (mockLogger.debug(_: String)) expects ("debug message")
-      (mockLogger.trace(_: String)) expects ("trace message")
-      (mockLogger.info(_: String)) expects ("info message")
-      (mockLogger.warn(_: String)) expects ("warn message")
-      (mockLogger.error(_: String)) expects ("error message")
+      val slf4jLogChute = new Slf4jLogChute
+      Whitebox.setInternalState(slf4jLogChute, "logger", mockLogger)
 
       slf4jLogChute.log(LogChute.DEBUG_ID, "debug message")
       slf4jLogChute.log(LogChute.TRACE_ID, "trace message")
       slf4jLogChute.log(LogChute.INFO_ID, "info message")
       slf4jLogChute.log(LogChute.WARN_ID, "warn message")
       slf4jLogChute.log(LogChute.ERROR_ID, "error message")
+
+      verify(mockLogger).debug("debug message")
+      verify(mockLogger).trace("trace message")
+      verify(mockLogger).info("info message")
+      verify(mockLogger).warn("warn message")
+      verify(mockLogger).error("error message")
     }
 
     it("log message with Throwable") {
       val mockLogger = mock[Logger]
 
-      val slf4jLogChute = new Slf4jLogChute {
-        logger = mockLogger
-      }
+      val slf4jLogChute = new Slf4jLogChute
+      Whitebox.setInternalState(slf4jLogChute, "logger", mockLogger)
 
       val th = new Throwable
-
-      (mockLogger.debug(_: String, _: Throwable)) expects ("debug message", th)
-      (mockLogger.trace(_: String, _: Throwable)) expects ("trace message", th)
-      (mockLogger.info(_: String, _: Throwable)) expects ("info message", th)
-      (mockLogger.warn(_: String, _: Throwable)) expects ("warn message", th)
-      (mockLogger.error(_: String, _: Throwable)) expects ("error message", th)
 
       slf4jLogChute.log(LogChute.DEBUG_ID, "debug message", th)
       slf4jLogChute.log(LogChute.TRACE_ID, "trace message", th)
       slf4jLogChute.log(LogChute.INFO_ID, "info message", th)
       slf4jLogChute.log(LogChute.WARN_ID, "warn message", th)
       slf4jLogChute.log(LogChute.ERROR_ID, "error message", th)
+
+      verify(mockLogger).debug("debug message", th)
+      verify(mockLogger).trace("trace message", th)
+      verify(mockLogger).info("info message", th)
+      verify(mockLogger).warn("warn message", th)
+      verify(mockLogger).error("error message", th)
     }
   }
 }
