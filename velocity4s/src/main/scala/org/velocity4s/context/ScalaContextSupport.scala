f811dbd15edc921978795c1cb3a21738d4f902c4
kazuhira-r <kazuhira.m@gmail.com>
2014-10-20 00:30:01 +0900

    Modified to resemble the scala.collection.mutable.Map, more ScalaContext

--- velocity4s/src/main/scala/org/velocity4s/context/ScalaContextSupport.scala
+++ velocity4s/src/main/scala/org/velocity4s/context/ScalaContextSupport.scala
@@ -6,29 +6,25 @@ trait ScalaContextSupport extends Context {
   def keys: Iterable[String] =
     getKeys.toIterable.asInstanceOf[Iterable[String]]
 
-  def +(elem: (String, AnyRef)): this.type = {
+  def +=(elem: (String, AnyRef), elems: (String, AnyRef)*): this.type = {
     put(elem._1, elem._2)
+    elems.foreach(e => put(e._1, e._2))
     this
   }
 
-  def ++(elems: (String, AnyRef)*): this.type =
-    this ++ elems
-
-  def ++(elems: Iterable[(String, AnyRef)]): this.type = {
-    elems.foreach(this + _)
+  def ++=(elems: Iterable[(String, AnyRef)]): this.type = {
+    elems.foreach(this += _)
     this
   }
 
-  def -(key: String): this.type = {
+  def -=(key: String, keys: String*): this.type = {
     remove(key)
+    keys.foreach(k => remove(k))
     this
   }
 
-  def --(keys: String*): this.type =
-    this -- keys
-
-  def --(keys: Iterable[String]): this.type = {
-    keys.foreach(this - _)
+  def --=(keys: Iterable[String]): this.type = {
+    keys.foreach(this -= _)
     this
   }
 }
